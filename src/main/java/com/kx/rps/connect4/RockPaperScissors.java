39eb2b08fc1190d74ab9fca2779ccb776cdf6ab6
xiongkhue <59174345+xiongkhue@users.noreply.github.com>
2021-03-01 21:53:45 -0600

    Rock Paper Scissor's Main, playGame() function.

---
+++ src/main/java/com/kx/rps/connect4/RockPaperScissors.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kx.rps.connect4;
+
+/**
+ *
+ * @author khuxi
+ */
+public class RockPaperScissors {
+    
+    public static void main(String[] args){
+        
+        //Execute game method
+        playGame();
+    }
+    
+    public static void playGame(){
+        //variazbles needed for keep tracking, and decisions
+        int rounds, wins, ties, loss, chosenOne, chosenTwo;
+        String playAgain, outCome, temp;
+        boolean play = true;
+    }
+    
+}
