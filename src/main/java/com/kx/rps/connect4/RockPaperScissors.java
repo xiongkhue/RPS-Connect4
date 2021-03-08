c11f9df81f2d9ecec66e9e8066d6975c18c99669
xiongkhue <59174345+xiongkhue@users.noreply.github.com>
2021-03-07 23:38:49 -0600

    Variables for Player(User and Program) Choices and scoring. Variables for runtime (# rounds, play again, still playing).

--- src/main/java/com/kx/rps/connect4/RockPaperScissors.java
+++ src/main/java/com/kx/rps/connect4/RockPaperScissors.java
@@ -5,6 +5,9 @@
  */
 package com.kx.rps.connect4;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author khuxi
@@ -18,10 +21,34 @@ public class RockPaperScissors {
     }
     
     public static void playGame(){
-        //variazbles needed for keep tracking, and decisions
-        int rounds, wins, ties, loss, chosenOne, chosenTwo;
-        String playAgain, outCome, temp;
-        boolean play = true;
+    //variazbles needed for keep tracking, and decisions
+    int wins, ties, loss, chosenOne, chosenTwo;
+    String outCome, temp;
+    //variazbles needed for runtime
+    int rounds;
+    String playAgain;
+    boolean play = true;
+    //RPS Game Choices : Rock, Paper, Scissors
+    String[] gesture = {"    _______\n" +
+        "---'   ____)\n" +
+        "      (_____)\n" +
+        "      (_____)\n" +
+        "      (____)\n" +
+        "---.__(___)", "    _______\n" +
+        "---'   ____)____\n" +
+        "          ______)\n" +
+        "          _______)\n" +
+        "         _______)\n" +
+        "---.__________)", "    _______\n" +
+        "---'   ____)____\n" +
+        "          ______)\n" +
+        "       __________)\n" +
+        "      (____)\n" +
+        "---.__(___)"};
+        
+    //Scanner for user input
+    //Random for program randomly choosing rock, paper, scissors
+    Scanner inputReader = new Scanner(System.in);
+    Random randomizer = new Random();
     }
-    
 }
