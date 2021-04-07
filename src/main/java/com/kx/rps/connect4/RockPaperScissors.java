40d5a55264e0c6fa48a3dd40d599ce14382a32f5
xiongkhue <59174345+xiongkhue@users.noreply.github.com>
2021-04-07 13:20:18 -0500

    Implemented user prompt and input for rounds and rock-paper-scissor choices. After input rounds is completed asks user if they want to play again.

--- src/main/java/com/kx/rps/connect4/RockPaperScissors.java
+++ src/main/java/com/kx/rps/connect4/RockPaperScissors.java
@@ -50,5 +50,50 @@ public class RockPaperScissors {
     //Random for program randomly choosing rock, paper, scissors
     Scanner inputReader = new Scanner(System.in);
     Random randomizer = new Random();
+    
+        //while User wants to play
+        while(play){
+            System.out.println("How many rounds would you like to play (1-10)?");
+            temp = inputReader.nextLine();
+            
+            //try and parse user input to integer value for rounds
+            try{
+                rounds = Integer.parseInt(temp);
+            }catch (NumberFormatException e) {
+                System.out.println("Ack! only 1-10 rounds allowed");
+                System.exit(0);
+            }
+            //if valid user input then initialize variables for game
+            rounds = Integer.parseInt(temp);
+            wins = 0;
+            loss = 0;
+            ties = 0;
+            
+            //for each round
+            for(int i = 0; i < rounds; i++){
+                System.out.println("Rock(1), Paper(2), Scissors(3), GO! [1-3]");
+                temp = inputReader.nextLine();
+                
+                //while user input is not valid, 1, 2, or 3, prompt user for valid input
+                while(!temp.equals("1") && !temp.equals("2") && !temp.equals("3")){
+                    System.out.println("Please enter 1, 2, or 3 only.");
+                    System.out.println("Rock(1), Paper(2), Scissors(3), GO! [1-3]");
+                    temp = inputReader.nextLine();
+                }
+                //once user input is valid  initialize user's choice and randomize program choice
+                chosenOne = Integer.parseInt(temp);
+                chosenTwo = 1 + randomizer.nextInt(3);
+            }
+            
+            //ask user if they want to play again
+            System.out.println("Play again? [y/n]");
+            playAgain = inputReader.nextLine();
+            
+            //ends game if player says no to playing again
+            if(playAgain.equals("n")){
+                play = false;
+                System.out.println("Thanks for playing!");
+            }
+        }
     }
 }
